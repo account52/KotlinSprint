01e9dcd3b8eef43954d02edec761b0458f536fd9
account52 <account52@yandex.ru>
2025-04-16 22:30:19 +0300

    KS-1-1 task1

--- src/main/kotlin/lesson1/lesson1_task1.kt
+++ src/main/kotlin/lesson1/lesson1_task1.kt
@@ -1,5 +1,7 @@
 package lesson1
 
 fun main(){
-    println("Hello!")
+    val totalOrders: Int = 75
+    val textAfterBuy: String = """Спасибо за покупку книги! 
+        |Пусть она подарит вам новые знания и вдохновит на изменения!""".trimMargin()
 }
