66c5709ed3063163ba1775047905aa2159fb2d87
account52 <account52@yandex.ru>
2025-04-17 12:13:55 +0300

    tav-1-2 Урок 1. Задача 2

---
+++ src/main/kotlin/lesson1/lesson1_task2.kt
@@ -0,0 +1,15 @@
+package lesson1
+
+fun main(){
+    val totalOrders: Int = 75
+    val textAfterBuy: String = """Спасибо за покупку!
+        |Мы рады, что вы выбрали наш магазин! Надеемся, что наши товары принесут вам радость и удовлетворят все ваши ожидания.""".trimMargin()
+
+    println(totalOrders)
+    println(textAfterBuy)
+
+    var countOfEmployees = 2000
+    //println("countOfEmployees = $countOfEmployees")
+    countOfEmployees -=1
+    println("countOfEmployees = $countOfEmployees")
+}
