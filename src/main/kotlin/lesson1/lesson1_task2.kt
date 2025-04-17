4844fe38a78c90eb5b8a29c530a045ca85ac080a
account52 <account52@yandex.ru>
2025-04-17 12:53:17 +0300

    tav-1-2 Урок 1. Задача 2

--- src/main/kotlin/lesson1/lesson1_task2.kt
+++ src/main/kotlin/lesson1/lesson1_task2.kt
@@ -3,7 +3,8 @@ package lesson1
 fun main() {
     val totalOrders: Int = 75
     val textAfterBuy: String = """Спасибо за покупку!
-        |Мы рады, что вы выбрали наш магазин! Надеемся, что наши товары принесут вам радость и удовлетворят все ваши ожидания.""".trimMargin()
+        |Мы рады, что вы выбрали наш магазин!
+        |Надеемся, что наши товары принесут вам радость и удовлетворят все ваши ожидания.""".trimMargin()
 
     println(totalOrders)
     println(textAfterBuy)
