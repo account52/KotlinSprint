018b5e995312fab301ae43e6017a17741108b19e
account52 <account52@yandex.ru>
2025-04-18 23:04:33 +0300

    ks-2-1 Урок 2. Задача 1

--- src/main/kotlin/lesson2/lesson2_task1.kt
+++ src/main/kotlin/lesson2/lesson2_task1.kt
@@ -8,7 +8,6 @@ fun main() {
     val scoreStudent3 = 3
     val scoreStudent4 = 5
     val result = (scoreStudent1 + scoreStudent2 + scoreStudent3 + scoreStudent4) / totalStudents
-
     println(String.format("%.2f", result.toDouble()))
 
 }
