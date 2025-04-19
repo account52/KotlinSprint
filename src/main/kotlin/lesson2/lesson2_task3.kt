2cad95b3e27cd859ca9ec1b406513bb5f2e4e882
account52 <account52@yandex.ru>
2025-04-19 18:00:10 +0300

    ks-2-3 Урок 2. Задача 3

--- src/main/kotlin/lesson2/lesson2_task3.kt
+++ src/main/kotlin/lesson2/lesson2_task3.kt
@@ -5,9 +5,9 @@ fun main() {
     val timeStart = "9:23"
     val duration = 457
 
-    val list: List<String> = timeStart.split(":")
-    val startHours = list[0].toInt()
-    val startMinutes = list[1].toInt()
+    val listPartsOfTime: List<String> = timeStart.split(":")
+    val startHours = listPartsOfTime[0].toInt()
+    val startMinutes = listPartsOfTime[1].toInt()
 
     val hoursOnWay = duration / MINUTES_IN_HOUR
     val minutesOnWay = duration - (hoursOnWay * MINUTES_IN_HOUR)
@@ -16,12 +16,16 @@ fun main() {
 
     var finishHours = startHours + hoursOnWay
 
-    if (finishMinutes >= 60) {
+    if (finishMinutes >= MINUTES_IN_HOUR) {
         finishHours += 1
-        finishMinutes -= 60
+        finishMinutes -= MINUTES_IN_HOUR
     }
 
-    println("finish time - ${finishHours.toString().padStart(2, '0')}:${finishMinutes.toString().padStart(2, '0')}")
+    val strHours = "%s".format(finishHours).padStart(2, '0')
+    val strMinutes = "%s".format(finishMinutes).padStart(2, '0')
+
+    println("finish time - $strHours:$strMinutes")
+
 }
 
 const val MINUTES_IN_HOUR = 60
