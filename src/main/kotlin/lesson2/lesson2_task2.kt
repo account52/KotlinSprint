e82e0bb74937b5c9563ceb4443afdd511f67b693
account52 <account52@yandex.ru>
2025-04-19 17:36:52 +0300

    ks-2-1 Урок 2. Задача 2

--- src/main/kotlin/lesson2/lesson2_task2.kt
+++ src/main/kotlin/lesson2/lesson2_task2.kt
@@ -8,18 +8,12 @@ fun main() {
     val countEmployees = 50
     val countInterns = 30
 
-    val sumEmployees = getSum(salaryEmployee, countEmployees)
-    val totalSum = sumEmployees + getSum(salaryIntern, countInterns)
+    val sumEmployees = salaryEmployee * countEmployees
+    val totalSum = sumEmployees + salaryIntern * countInterns
     val averageSum = totalSum / (countEmployees + countInterns)
 
     println(sumEmployees)
     println(totalSum)
     println(averageSum)
 
-
-}
-
-fun getSum(a: Int, b: Int): Int {
-
-    return a * b
 }
