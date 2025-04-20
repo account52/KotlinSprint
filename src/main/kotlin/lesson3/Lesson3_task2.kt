ecf251ed26276b9d6114e839e6c9218b723645c0
account52 <account52@yandex.ru>
2025-04-20 10:12:57 +0300

    KS-3-2

---
+++ src/main/kotlin/lesson3/Lesson3_task2.kt
@@ -0,0 +1,17 @@
+package lesson3
+
+fun main() {
+
+    val fistAge = 20
+    val secondAge = 22
+
+    val name = "Татьяна"
+    val patronymic = "Сергеевна"
+
+    val firstSurname = "Андреева"
+    val secondSurname = "Сидорова"
+
+    println("$firstSurname $name $patronymic, $fistAge")
+    println("$secondSurname $name $patronymic, $secondAge")
+
+}
