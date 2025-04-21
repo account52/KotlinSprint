fecf9337b4217c0418e68b8b49aa1d11c6d8dd97
account52 <account52@yandex.ru>
2025-04-21 22:11:10 +0300

    KS-3-4

---
+++ src/main/kotlin/lesson3/lesson3_task4.kt
@@ -0,0 +1,18 @@
+package lesson3
+
+fun main() {
+
+    var stepFrom = "E2"
+    var stepTo = "E4"
+    var stepNumber = 1
+
+    println("$stepFrom-$stepTo;$stepNumber")
+
+    stepFrom = "D2"
+    stepTo = "D3"
+    stepNumber = 2
+
+    println("$stepFrom-$stepTo;$stepNumber")
+
+}
+
