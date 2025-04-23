c8db1f16631abbe71ef2cf6cac7a40434d5a4d3c
account52 <account52@yandex.ru>
2025-04-23 12:30:59 +0300

    KS-4-4

--- src/main/kotlin/lesson4/lesson4_task4.kt
+++ src/main/kotlin/lesson4/lesson4_task4.kt
@@ -3,13 +3,13 @@ package lesson4
 fun main() {
 
     val dayNumber = 5
-    val doGroup1 = (dayNumber % 2 != 0)
-    val doGroup2 = (dayNumber % 2 == 0)
 
-    println("""Упражнения для рук:    $doGroup1
-    |Упражнения для ног:    $doGroup2    
-    |Упражнения для спины:  $doGroup2
-    |Упражнения для пресса: $doGroup1
+    val isEven = (dayNumber % 2 == 0)
+
+    println("""Упражнения для рук:    ${!isEven}
+    |Упражнения для ног:    $isEven    
+    |Упражнения для спины:  $isEven
+    |Упражнения для пресса: ${!isEven}
     """.trimMargin())
 
 }
